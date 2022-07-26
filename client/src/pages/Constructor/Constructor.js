b0d572c3d92c339f1485b82643521e55ef8bdac9
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-07-26 11:22:21 +0300

    under control

--- client/src/pages/Constructor/Constructor.js
+++ client/src/pages/Constructor/Constructor.js
@@ -7,7 +7,7 @@ const Constructor = () => {
   return (
     <div className='constructor'>
         <div className='container'>
-            <h1>It is Constructor</h1>
+            <h1>This page is under construction</h1>
             <img className='constructor-img' src={img}></img>
         </div>
     </div>
