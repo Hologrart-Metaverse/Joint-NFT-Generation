0e2e5727b45e42f011e4d0b7ee7d6fccc4901828
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-08-21 10:14:38 +0300

    cors arrangements

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors()); // { origin: "http://localhost:3000" }
+// app.use(cors({ origin: "http://localhost:3000" })); 
+app.use(cors({ origin: "https://joint-nft-generation.vercel.app/" })); 
 
 const CONNECTION_URL = process.env.CONNECTION;
 const PORT = process.env.PORT || 8080;
