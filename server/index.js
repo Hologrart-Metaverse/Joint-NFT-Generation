3a50201378b16f9b98f6bd877ec605d6a86d6847
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-08-21 10:59:26 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const env = dotenv.config();
 
 import userRoutes from "./routes/User.js";
 import canvasesRoutes from "./routes/Canvas.js";
-import Canvases from "./models/Canvases.js";
 
 const app = express();
 
@@ -16,14 +15,20 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // app.use(cors({ origin: "http://localhost:3000" })); 
 // app.use(cors({ origin: "https://joint-nft-generation.vercel.app" })); 
-app.use(cors());
-app.use( (req, res, next) => {
+app.use(cors((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://joint-nft-generation.vercel.app'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-});
+}));
+// app.use( (req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://joint-nft-generation.vercel.app'); 
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// })
 
 const CONNECTION_URL = process.env.CONNECTION;
 const PORT = process.env.PORT || 8080;
@@ -35,12 +40,3 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
 
 app.use("/user", userRoutes);
 app.use("/canvas", canvasesRoutes);
-
-app.get("/canvas/get", async (req, res) => {
-    try {
-        const canvases = await Canvases.find();
-        res.status(200).json(canvases.reverse());
-    } catch (error) {
-        res.status(404).json({ message: error.message});
-    }
-})
