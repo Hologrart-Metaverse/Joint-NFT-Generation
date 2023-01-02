b36bdc5231e5a157406c8a39e1357ec6140858ba
DogukanTopcu <dogukan_topcu3575@icloud.com>
2023-01-02 22:36:09 +0300

    site is normalized

--- client/src/components/Draw/components/Editor.js
+++ client/src/components/Draw/components/Editor.js
@@ -8,6 +8,7 @@ import { useSelector } from 'react-redux';
 
 const Editor = ({ canvasPixels, canvas_id, setBuyAlert }) => {
     const user = useSelector((state) => state.user);
+    const [width, setWidth] = useState(window.innerWidth);
     useEffect(() => {
       if (user.length !== 0) {
         setSelectedColor("#ffffff");
@@ -17,7 +18,9 @@ const Editor = ({ canvasPixels, canvas_id, setBuyAlert }) => {
       )
     }, [user]);
 
-
+    useEffect(() => {
+      setWidth(window.innerWidth);
+    }, [window.innerWidth]);
 
     const [selectedColor, setSelectedColor] = useState("none");
 
@@ -28,16 +31,20 @@ const Editor = ({ canvasPixels, canvas_id, setBuyAlert }) => {
     
   return (
     <div className='drawEditor'>
-      {/* {
-        user.length !== 0 ? (<SketchPicker className='sketchPicker' color={selectedColor} onChange={changeColor} />) : null
-      } */}
-      <HuePicker className='sketchPicker' color={selectedColor} onChange={changeColor} />
+      {
+        user.length !== 0 ? (
+          width > 500 ? (
+            <SketchPicker className='sketchPicker' color={selectedColor} onChange={changeColor} />
+          ) : (<HuePicker className='huePicker' color={selectedColor} onChange={changeColor} />)
+        ) : null
+      }
+      
       {
         // canvasPixels[1] ? <DrawingPanel className='DrawingPanel' width={canvasPixels[1].length} height={canvasPixels.length} selectedColor={selectedColor} /> : null
         canvasPixels[1] ? <DrawingPanel className='DrawingPanel' setBuyAlert={setBuyAlert} pixels={canvasPixels} canvas_id={canvas_id} selectedColor={selectedColor} /> : null
       }
 
-      <p className='bottom-message'>Bir renk seçerek pixeli boya ve eseri oluşturmaya katkı sağla 🚀</p>
+      {/* <p className='bottom-message'>Bir renk seçerek pixeli boya ve eseri oluşturmaya katkı sağla 🚀</p> */}
         
     </div>
   )
